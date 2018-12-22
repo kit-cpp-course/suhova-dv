5ba67f39879d03e1891800ce01fe9aa4988a7b77
Darya Suhova <45993488+suhova@users.noreply.github.com>
2018-12-22 08:31:41 +0300

    Update Median.cpp

--- Median.cpp
+++ Median.cpp
@@ -1,50 +1,51 @@
 #include "Median.h"
-#include "QuickSort.h"
 
 using namespace filters;
 using namespace colorsort;
 
 void Median::filtration(CImage img, size_t p, LPCSTR output) {
 		
-	size_t X, Y, size, mediana, imax,jmax;
+	p = power;
+	size = (2 * p + 1)*(2 * p + 1);
+	size_t X, Y,imax,jmax;
 	X = img.GetWidth();
 	Y = img.GetHeight();
-	size = (2 * p + 1)*(2 * p + 1);
 	if (size < X * Y) {
 		mediana = size / 2 + 1;
 		imax = X - p;
 		jmax = Y - p;
 		HDC hdc = img.GetDC();
-		QuickSort qs;
-		COLORREF** arr = Arrarator(hdc, X, Y);
-		COLORREF* array = new COLORREF[size + 1];
+		COLORREF** const arr = Arrarator(hdc, X, Y);
 
 		for (int i = p;i < imax; i++)
 		{
 			for (int j = p;j < jmax;j++)
 			{
-				int l = i - p, r = i + p, f = j + p, c = j - p;
-				int count = 0;
-				for (int n = l;n <= r;n++)
-					for (int k = c;k <= f;k++) {
-						array[count] = arr[n][k];
-						count++;
-					}
-				qs.sort(array, 0, count);
-				img.SetPixel(i, j, array[mediana]); 
+				Window(hdc, i, j, arr);
 			}
 		}
-
 		img.Save(output);
-
 		for (int i = 0; i < X; i++) { delete[] arr[i]; }
 		delete[] arr;
-		delete[] array;
 		img.ReleaseDC();
 		img.Destroy();
 	}
 }
 
+void Median::Window( HDC hdc, int i, int j,  COLORREF** const arr) {
+	COLORREF* array = new COLORREF[size + 1];
+	QuickSort qs;
+	int l = i - p, r = i + p, f = j + p, c = j - p;
+	int count = 0;
+	for (int n = l;n <= r;n++)
+		for (int k = c;k <= f;k++) {
+			array[count] = arr[n][k];
+			count++;
+		}
+	qs.sort(array, 0, count);
+	SetPixel(hdc, i, j, array[mediana]);
+	delete[] array;
+}
 
 COLORREF** Median::Arrarator(HDC hdc, size_t X, size_t Y) {
 	COLORREF** arr = new COLORREF*[X];
