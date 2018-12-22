9e69647dff66ced5ccb25318373667605e19270b
Darya Suhova <45993488+suhova@users.noreply.github.com>
2018-12-22 09:01:32 +0300

    Update QuickSort.h

--- QuickSort.h
+++ QuickSort.h
@@ -2,10 +2,16 @@
 #include "Sort.h"
 
 namespace colorsort {
-
+        /*
+	* Класс, реализующий быструю сортировку цветов
+	*/
 	class QuickSort :public Sort{
 	public:
-		QuickSort() {};
+		/*
+		* Быстрая сортировка одномерного массива COLORREF * array
+		* left - индекс первого элемента сортировки
+		* right - индекс последнего элемента сортировки
+		*/
 		void sort(COLORREF* array, int left, int right);
 	};
 }
