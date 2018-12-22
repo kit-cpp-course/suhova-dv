e6cbeea93b6c0463f60a53b4ad008d67301337f1
Darya Suhova <45993488+suhova@users.noreply.github.com>
2018-12-22 08:38:31 +0300

    Update Filter.h

--- Filter.h
+++ Filter.h
@@ -2,9 +2,15 @@
 #include <atlimage.h>
 
 namespace filters {
-
-	class Filter {
+/*
+* Абстракный класс над всевозможными алгоритмами фильтрации изображений
+*/
+	class Filter {	
 	public:
+		/*
+		* Виртуальная функция, фильтрующая изображение img с мощностью p.
+                * LPCSTR output - путь сохранения результата
+		*/
 		virtual void filtration(CImage img, size_t p, LPCSTR output) = 0;
 	};
 }
