f32c85721c08a70a9fd25ca1f63c960429b4a916
Darya Suhova <45993488+suhova@users.noreply.github.com>
2018-12-22 08:57:39 +0300

    Update Sort.h

--- Sort.h
+++ Sort.h
@@ -2,9 +2,16 @@
 #include <atlimage.h>
 
 namespace colorsort {
-
+        /*
+	* Абстракный класс над всевозможными сортировками цветов
+	*/
 	class Sort {
 	public:
+		/*
+		* Сортировка одномерного массива COLORREF * array
+		* left - индекс первого элемента сортировки
+		* right - индекс последнего элемента сортировки
+		*/
 		virtual void sort(COLORREF * array, int left, int right) = 0;
 	};
 }
