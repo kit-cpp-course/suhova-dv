863c6ab769107d38fff827aab2f3ce96ddb57f75
Darya Suhova <45993488+suhova@users.noreply.github.com>
2018-12-23 23:05:28 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,57 +3,21 @@
 #include <string>
 #include <fstream>
 #include <regex>
+#include "Arguments.h"
 
 using namespace filters;
 using namespace std;
 
-int main() {
+int main(size_t count, char ** args) {
 	setlocale(LC_ALL, "ru");
-	
-	std::string out, in;
-
-	cout << " Enter the source image address: ";
-	getline(cin, in);
-	char const * c_in=in.c_str();
-	ifstream f(c_in);
-	if (f.good()) {
-		if (!regex_match(in.begin(), in.end(), regex{ R"([^\s]+((jpg|png|gif|bmp))$)" }))
-		{
-			cout << endl << "Error.";
-			system("pause");
-			return 0;
-		}
-	}else {
-		cout << endl << "Error."<<endl;
-		system("pause");
-		return 0;
-	}
-	LPCSTR input(c_in);
-
-	cout << endl << " Enter the save path: ";
-	getline(cin, out);
-	char const * c_out = out.c_str();
-	if (!regex_match(out.begin(), out.end(), regex{ R"([^\s]+((jpg|png|gif|bmp))$)" }))
-	{
-		cout << endl << "Error.";
-		system("pause");
-		return 0;
-	}
-	LPCSTR output(c_out);
-
-	size_t p=0;
-	cout << endl << " Enter filtering power (1 - 10) : ";
-	cin >> p;
-	if (!p) {
-		cout << endl << "Error.";
-		system("pause");
-		return 0;
-	}
+	config conf = config::instance();
+	cmd::arguments argcheck(count, args);
+	argcheck.ArgControl(conf);
 
 	CImage img;
-	img.Load(input);
+	img.Load(conf.input());
 	Median med;
-	med.filtration(img, p, output);
+	med.filtration(img, conf.power(), conf.output());
 		
 	return 0;
 }
